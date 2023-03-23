0b161c027b51174d1e59de99ce79417e9ed73004
Unnikrishnan Menon <menon.uk1998@gmail.com>
2023-03-23 16:33:43 +0530

    resolved transaction issues across staked, available and stashed tokens
    Co-authored-by: Anirudh <senseianirudh28@gmail.com>

--- scheduler/utils.py
+++ scheduler/utils.py
@@ -1,10 +1,10 @@
 import json
 import os
 import pickle as pkl
-
+import requests
 import numpy as np
-
-from .config import DATA_FILES_PATH
+import logging
+from .config import DATA_FILES_PATH, RAVENAUTH_URL, RAVENVERSE_SUPERUSER_TOKEN, RAVENVERSE_SUPERUSER_USERNAME, RAVENVERSE_SUPERUSER_PASSWORD
 
 
 def load_data_from_file(file_path, np=None):
@@ -36,3 +36,23 @@ def save_data_to_file(data_id, data):
         json.dump(data, f)
 
     return file_path
+
+def get_access_token():
+    response = requests.post(url="{}api/token/".format(RAVENAUTH_URL),
+                             data={"username": RAVENVERSE_SUPERUSER_USERNAME,
+                                   "password": RAVENVERSE_SUPERUSER_PASSWORD})
+    if response.status_code == 200:
+        return response.json()['access']
+    else:
+        logging.debug(response.text)
+        return None
+
+def verify_and_get_access_token():
+    response = requests.post(url="{}/api/token/verify/".format(RAVENAUTH_URL),
+                             data={"token": RAVENVERSE_SUPERUSER_TOKEN})
+
+    if response.status_code == 200:
+        return RAVENVERSE_SUPERUSER_TOKEN
+    else:
+        logging.debug("Invalid access token, getting new access token")
+        return get_access_token()
